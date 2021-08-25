59683fa327ac2f66d9864f24b912dd0644eb34a0
r00l <no-reply@gmail.com>
2021-08-25 21:21:48 +0200

    add filewriter util

---
+++ airdropper/utils.py
@@ -0,0 +1,9 @@
+import json
+import uuid
+
+
+def dict_to_file(input: dict, fname: str) -> bool:
+    fname += f"_{str(uuid.uuid1())[:3]}"
+    with open(f"{fname}.json", 'w') as json_file:
+        json.dump(input, json_file, indent=4)
+        return True
